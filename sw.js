d3ceb0a82da57090002dda1fab8587bc1f78bcda
kentjanuar <c14220124@john.petra.ac.id>
2025-03-23 17:23:11 +0700

    fix fix

--- sw.js
+++ sw.js
@@ -1,69 +1,50 @@
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  self.skipWaiting(); // Force update on new version
+const CACHE_NAME = 'pwa-cache-v3';
+const DYNAMIC_CACHE = 'pwa-dynamic-cache';
+const OFFLINE_PAGE = '/offline.html';
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installing...');
   event.waitUntil(
-    caches.open('static')
-      .then(function(cache) {
-        console.log('[Service Worker] Precaching App Shell');
-        return cache.addAll([  
-          '/',
-          '/index.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-          '/offline.html', 
-          '/src/css/offline.css'
-        ]);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Pre-caching assets');
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/offline.html',
+        '/src/css/app.css',
+        '/src/js/app.js',
+        '/manifest.json',
+        '/src/css/offline.css'
+      ]);
+    })
   );
+  self.skipWaiting();
 });
 
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-              return cachedResponse; // Serve from cache first
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activating...');
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME && key !== DYNAMIC_CACHE) {
+            console.log('[Service Worker] Deleting old cache:', key);
+            return caches.delete(key);
           }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
 
-          return fetch(event.request).catch(() => {
-              // If request is for a navigation (HTML page), return the offline fallback
-              if (event.request.mode === "navigate") {
-                  return caches.match("/offline.html"); 
-              }
-          });
-      })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).catch(() => caches.match('/offline.html'));
+    })
   );
 });
 
 
 
-// self.addEventListener('activate', function(event) {
-//   console.log('[Service Worker] Activating Service Worker ....', event);
-//   return self.clients.claim();
-// });
-
-
-
-  // self.addEventListener('fetch', function(event) {
-  //   event.respondWith(
-  //     caches.open(CACHE_DYNAMIC_NAME)
-  //       .then(function(cache) {
-  //         return fetch(event.request)
-  //           .then(function(res) {
-  //             cache.put(event.request, res.clone());
-  //             return res;
-  //           });
-  //       })
-  //   );
-  // });
-  
-  
